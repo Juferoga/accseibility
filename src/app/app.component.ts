645cae663f1a92ff528212e90d863b084bc3361d
Juferoga <juancholoco2000.61@gmail.com>
2024-10-07 02:18:54 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccessibilityComponent } from './shared/componente-accesibilidad.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AccessibilityComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
